6394e7b423ae63561d5e1cf98699f5a9d900108e
Vincent Nys <vincent.nys@ap.be>
2023-03-14 12:24:21 +0100

    Interesting use of multiple impl blocks here. Revisit later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{collections::hash_map::RandomState, hash::BuildHasher};
+
+const INITIAL_NBUCKETS: usize = 1; // for easier testing
+
+struct Bucket<K, V> {
+    items: Vec<(K, V)>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// S is a a way to build a hasher
+pub struct HashMap<K, V, S = RandomState> {
+    buckets: Vec<Bucket<K, V>>,
+    build_hasher: S, // would've thought hasher itself...
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<K, V> HashMap<K, V, RandomState> {
+    pub fn new() -> Self {
+        HashMap {
+            buckets: vec![], // start empty to avoid allocating when it is not necessary
+            build_hasher: RandomState::new(),
+        }
+    }
+}
+
+impl<K, V, S> HashMap<K, V, S>
+where
+    S: BuildHasher,
+{
+    fn resize(&mut self) {
+        let target_size = match self.buckets.len() {
+            0 => INITIAL_NBUCKETS,
+            n => 2 * n,
+        };
+        // TODO
+    }
+
+    pub fn insert(&mut self, key: K, value: V) -> Option<V> {
+        key.hash() % self.buckets.len()
     }
 }
